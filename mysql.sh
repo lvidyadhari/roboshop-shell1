90468180bd2e5dfbac9b84095f4c7375d9c7df95
lvidyadhari <lathavidyadhari97@gmail.com>
2023-12-28 00:33:14 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -5,12 +5,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.daws76s.store
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -30,26 +29,27 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf module disable mysql -y &>> $LOGFILE
+dnf module disable mysql -y  &>> $LOGFILE
 
-VALIDATE $? "Disable current MySQL version"
+VALIDATE $? "Disabling current mysql version"
 
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>> $LOGFILE
 
-VALIDATE $? "Copied MySQl repo"
+VALIDATE $? "Copied Mysql repo"
 
 dnf install mysql-community-server -y &>> $LOGFILE
 
-VALIDATE $? "Installing MySQL Server"
+VALIDATE $? "Installing MySQL server"
 
-systemctl enable mysqld &>> $LOGFILE 
+systemctl enable mysqld  &>> $LOGFILE
 
-VALIDATE $? "Enabling MySQL Server"
+VALIDATE $? "Enabling mysqld"
 
 systemctl start mysqld &>> $LOGFILE
 
-VALIDATE $? "Starting  MySQL Server" 
+VALIDATE $? "Starting mysqld"
 
 mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
 
-VALIDATE $? "Setting  MySQL root password"
+VALIDATE $? "Setting mysql root password"
+
---
+++ shipping.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install maven -y
+
+VALIDATE $?
+
+useradd roboshop
+
+id roboshop #if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+
+cd /app
+
+VALIDATE $?
+
+unzip /tmp/shipping.zip
+
+cd /app
+
+VALIDATE $?
+
+mvn clean package
+
+VALIDATE $?
+
+mv target/shipping-1.0.jar shipping.jar
+
+
+cp /home/centos/roboshop-shell1/shipping.service /etc/systemd/system/shipping.service
+
+VALIDATE $?
+
+systemctl daemon-reload
+
+VALIDATE &? "Realoading file"
+
+systemctl enable shipping 
+
+VALIDATE $?
+
+systemctl start shipping
+
+VALIDATE $?
+
+dnf install mysql -y
+
+VALIDATE $? "Installing Mysql"
+
+mysql -h mysql.daws76s.store -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+
+systemctl restart shipping
+
+VALIDATE $? "Restarted shipping"
