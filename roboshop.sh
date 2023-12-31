3167a3ed0a855393361d753c4c09c6371e6e32c2
lvidyadhari <lathavidyadhari97@gmail.com>
2023-12-31 23:54:45 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -27,11 +27,11 @@ do
         ,"Changes": [{
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "$i.$DOMAIN_NAME"
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "$IP_ADDRESS"
+                "Value"         : "'$IP_ADDRESS'"
             }]
         }
         }]
