34ea7710097f032d080a9a259d285fac7d1a61c2
lvidyadhari <lathavidyadhari97@gmail.com>
2024-01-02 12:09:28 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-AMI=ami-03265a0778a880afb
-SG_ID=sg-0edd3d40dafc99687
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z028577132BYH8QZFT8FZ # replace your zone ID
-DOMAIN_NAME="daws76s.store"
+INSTANCES_NAME=("mongodb" "mysql" "redis" "catalogue" "user" "cart" "shipping" "payments" "rabbitmq" "dispatch" "web")
+INSTANCE_TYPE=""
+DOMAIN_PATH="daws76s.store"
+HOSTED_ZONE_ID="Z028577132BYH8QZFT8FZ"
 
-for i in "${INSTANCES[@]}"
-do 
-  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-  then 
-       INSTANCE_TYPE="t3.small"
+for i in "${INSTANCES_NAME[@]}"
+do
+    if [[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]]; then
+    INSTANCE_TYPE="t3.small"
     else
-       INSTANCE_TYPE="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE  --security-group-ids sg-0edd3d40dafc99687 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-    --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
 
-    #CREATE R53 record, make sure you delete existing record
+    PRIVATE_IPADDRESS=$(aws ec2 run-instances \
+                    --image-id ami-03265a0778a880afb \
+                    --count 1 \
+                    --instance-type "$INSTANCE_TYPE" \
+                    --security-group-ids sg-0edd3d40dafc99687 \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
+                    --query 'Instances[0].PrivateIpAddress' \
+                    --output text)
+    echo "$i : $PRIVATE_IPADDRESS" 
     aws route53 change-resource-record-sets \
-   --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
-        }
+    --hosted-zone-id "$HOSTED_ZONE_ID" \
+    --change-batch "{
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$i.$DOMAIN_PATH\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{\"Value\": \"$PRIVATE_IPADDRESS\"}]
+            }
         }]
-    }
-
-    
+    }"
+    echo "record created successfully for $i : $i.$DOMAIN_PATH"
 done
